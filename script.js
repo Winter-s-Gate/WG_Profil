0ae3b780d94903e2e27337463093f06de93e01b9
Winter's Gate <wintersgatesl@gmail.com>
2025-10-22 10:27:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("https://wgprofil.wintersgatesl.workers.dev/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ uuid })
+        body: JSON.stringify({ uuid, action: "get" })
     })
     .then(res => res.text())
     .then(text => {
